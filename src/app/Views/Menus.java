b27f2a6b57a195ba7af759c2e9dfdee46242c104
Brunovski28 <bruno.procamilo@gmail.com>
2023-05-15 20:02:17 -0300

    Arrumando select, insert e arrumando menu

--- src/app/Views/Menus.java
+++ src/app/Views/Menus.java
@@ -5,14 +5,16 @@ public class Menus {
         System.out.println( "\r ===================** CrudAsk **==================== \r\n");
 
         System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
-        System.out.println( "\r Utilize as numerções para usar as funcionalidades \r\n");
+        System.out.println( "\r Utilize as numerações para usar as funcionalidades \r\n");
 
         String[] options = { "Clientes", "Produto"};
         
-        for (int i = 1; i <= 2; i++) {
+       
+        int i = 0;
             for (String item : options) {
-                System.out.println("[" + i++ + "]" + item); 
+                System.out.println("[" + (i+1) + "] " + item); 
+                i++;
             }
-        }
+       
     }
 }
