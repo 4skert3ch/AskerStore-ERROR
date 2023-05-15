08a934570cde6cbf95d1d41a4a47a1fcd3face7b
cauallr <132109162+cauallr@users.noreply.github.com>
2023-05-15 20:17:25 -0300

    CYC#4
    Menu clientes

--- src/app/Views/Menus.java
+++ src/app/Views/Menus.java
@@ -5,7 +5,7 @@ public class Menus {
         System.out.println( "\r ===================** CrudAsk **==================== \r\n");
 
         System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
-        System.out.println( "\r Utilize as numerções para usar as funcionalidades \r\n");
+        System.out.println( "\r Utilize as numerações a seguir para usar as funcionalidades \r\n");
 
         String[] options = { "Clientes", "Produto"};
         
@@ -15,4 +15,21 @@ public class Menus {
             }
         }
     }
+
+    public static void MenuClientes() {
+        System.out.println( "\r ===================** Menu Clientes**==================== \r\n");
+
+        System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
+        System.out.println( "\r Utilize as numerações a seguir para usar as funcionalidades: \r\n");
+
+        String[] options = { "Cadastrar", "Atualizar", "Excluir", "Exibir"};
+        
+        for (int i = 1; i <= 4; i++) {
+            for (String item : options) {
+                System.out.println("[" + i++ + "]" + item); 
+            }
+        }
+    }
+
+    
 }
