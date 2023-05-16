697ad38706cf0643d162700223a87276a4fc2af5
João Victor F. Arruda <71611166+joaoarruda101@users.noreply.github.com>
2023-05-16 12:33:35 -0300

    CYC#Admin - Mesclando alteração

--- src/app/App.java
+++ src/app/App.java
@@ -9,22 +9,18 @@ public class App {
     
     public static void main(String[] arg) throws Exception {
         Conexao conn = new Conexao();
-        conn.main(arg);
         Menus menu = new Menus();
+
+        conn.main(arg);
 
         Scanner scan = new Scanner(System.in);
         String escolha = scan.nextLine();
 
-        if(escolha != "1"){
-
+        if(escolha == "1"){
             menu.MenuClientes();
-
-        } else if(escolha != "2"){
-
+        }
+        if(escolha == "2"){
             //Menu Produtos
-            
-        }else{
-            System.out.println("FODASE");
         }
 
     }
