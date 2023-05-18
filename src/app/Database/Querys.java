86a1d35b00361ee966cced6881edc8a3e829f017
Gabriel Faustino Poleza - 152310680 <152310680@ulife.com.br>
2023-05-18 19:45:24 -0300

    CYC# - Método RetornaCamposTabela

--- src/app/Database/Querys.java
+++ src/app/Database/Querys.java
@@ -1,8 +1,12 @@
 package app.Database;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 public class Querys {
     /**
@@ -20,6 +24,26 @@ public class Querys {
         stmt.executeUpdate(sql);
 
         return stmt;
+    }
+
+    public static ArrayList<String> RetornaCamposTabela(Connection conn, String tabela ) throws SQLException {
+        Statement stmt = conn.createStatement();
+
+        ResultSet rs = null;
+
+        ArrayList<String> campos = new ArrayList();
+
+        String sql;
+        sql = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS" +
+              "WHERE TABLE_NAME = " + tabela;
+        
+        rs = stmt.executeQuery(sql);      
+
+        while (rs.next()) {                
+            campos.add(rs.getString("column_names"));
+        }
+
+        return campos;
     }
 
     // public static Statement Consultas(Connection conn ) throws SQLException {
