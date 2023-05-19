5e68f6c1531dde43aa3befb8f82330c7123b16a0
Vinícius Rampelotti <viniciusrampeloti@gmail.com>
2023-05-19 19:05:59 -0300

    CYC#5 - Import de bibliotecas e adição de menu produtos

--- src/app/Views/Menus.java
+++ src/app/Views/Menus.java
@@ -1,7 +1,14 @@
 package app.Views;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Menus {
     public static void MenuPrincipal() {
+
         System.out.println( "\r ===================** CrudAsk **==================== \r\n");
 
         System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
@@ -18,6 +25,21 @@ public class Menus {
 
     public static void MenuClientes() {
         System.out.println( "\r =================** Menu Clientes **================== \r\n");
+
+        System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
+        System.out.println( "\r Utilize as numerções para usar as funcionalidades \r\n");
+
+        String[] options = { "Criar", "Pesquisar", "Atualizar", "Deletar"};
+        
+        for (int i = 1; i <= 4; i++) {
+            for (String item : options) {
+                System.out.println("[" + i++ + "]" + item); 
+            }
+        }
+    }
+
+    public static void MenuProdutos() {
+        System.out.println( "\r =================** Menu Produtos **================== \r\n");
 
         System.out.println( "\r ( SQUAD : A Computaria | COMPANHIA : ÁskerTech ) \r");
         System.out.println( "\r Utilize as numerções para usar as funcionalidades \r\n");
