06f232c81f368204c2300c26231f74e763cf2318
João Victor F. Arruda <71611166+joaoarruda101@users.noreply.github.com>
2023-05-19 19:44:49 -0300

    CYC#OWNER - classes bin

--- src/app/Views/Menus.java
+++ src/app/Views/Menus.java
@@ -1,11 +1,5 @@
 package app.Views;
 
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 public class Menus {
     public static void MenuPrincipal() {
 
