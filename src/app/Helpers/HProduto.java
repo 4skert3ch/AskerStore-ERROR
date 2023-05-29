6bf9120d6ce8462026d00a31c99f86786e40138d
cauallr <132109162+cauallr@users.noreply.github.com>
2023-05-29 15:29:11 -0300

    CYC#43
    Arrumado o printSelect de Hproduto

--- src/app/Helpers/HProduto.java
+++ src/app/Helpers/HProduto.java
@@ -3,7 +3,7 @@ package app.Helpers;
 import java.util.ArrayList;
 public class HProduto {
     public static void printaSelect(ArrayList<String> select) {
-        int tamanho = ((select.size()) / 7);
+        int tamanho = ((select.size()) / 4);
         System.out.println(tamanho);
     }
 }
